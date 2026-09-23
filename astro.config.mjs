24aff4797696079b3bb57eb77600aa9d6268361c
Juana Aldana <juvaalru@gmail.com>
2026-09-23 11:47:25 -0500

    Configurar sitio estático para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,25 +5,16 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
-import keystatic from "@keystatic/astro";
-import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), markdoc(), keystatic()],
+  site: "https://imjv08.github.io",
+
+  integrations: [react(), markdoc()],
 
   vite: {
     plugins: [tailwindcss()],
-    optimizeDeps: {
-      include: ["@keystatic/core", "@keystatic/astro"],
-    },
   },
 
-  output: "server",
-
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
+  output: "static",
 });
